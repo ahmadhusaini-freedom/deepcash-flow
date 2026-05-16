692bc176ef9eb67f9d3222f07c2acf96387bc3b3
ahmadhusaini-freedom <mr.ahmadhusaini77@gmail.com>
2026-05-16 11:34:25 +0800

    Add files via upload

---
+++ deepseek_bash_20260516_3439ae.sh
@@ -0,0 +1,6 @@
+git init
+git checkout -b main
+git add .
+git commit -m "First commit - DeepCashFlow system"
+git remote add origin https://github.com/USER/deepcash-flow.git   # ganti USER dengan username GitHub Anda
+git push -u origin main
